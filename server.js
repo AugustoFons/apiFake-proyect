35a1c7c1cf82dc2548810320924e2405bc992373
Augusto Fons <augustofonsdev@gmail.com>
2023-02-10 01:08:38 -0300

    prueba yarn

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+const clone = require('clone')
+const data = require('./productos.json')
+
+const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create()
-const router = jsonServer.router('productos.json')
+
+// For mocking the POST request, POST request won't make any changes to the DB in production environment
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'productos.json', {
+    _isFake: isProductionEnv
+})
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
+
+server.use((req, res, next) => {
+    if (req.path !== '/')
+        router.db.setState(clone(data))
+    next()
 })
 
-// Export the Server API
-module.exports = server
+server.use(router)
+server.listen(process.env.PORT || 8000, () => {
+    console.log('JSON Server is running')
+})
 
 // Export the Server API
 module.exports = server
