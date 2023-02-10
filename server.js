00cb7fdbbfb403fdafba971217e3f3720368a3b3
Augusto Fons <augustofonsdev@gmail.com>
2023-02-10 20:12:55 -0300

    intt3

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const jsonServer = require('json-server')
-const server = jsonServer.create()
 const clone = require('clone')
 const data = require('./productos.json')
 
+const isProductionEnv = process.env.NODE_ENV === 'production';
+const server = jsonServer.create()
+
 // For mocking the POST request, POST request won't make any changes to the DB in production environment
-const router = jsonServer.router('productos.json')
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'productos.json', {
+    _isFake: isProductionEnv
+})
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
 
 server.use((req, res, next) => {
     if (req.path !== '/')
