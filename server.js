6d56293d9ab3f3651c2af21fc2df737ad9a5eca2
Augusto Fons <augustofonsdev@gmail.com>
2023-02-10 20:22:00 -0300

    motorola

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const jsonServer = require('json-server')
+/* const jsonServer = require('json-server')
 const clone = require('clone')
 const data = require('./productos.json')
 
@@ -21,6 +21,25 @@ server.use((req, res, next) => {
 
 server.use(router)
 server.listen(process.env.PORT || 8000, () => {
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server */
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./productos.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(5000, () => {
     console.log('JSON Server is running')
 })
 
